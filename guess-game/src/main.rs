34fb9bfc73bb7ecfd54b1626c8e3889454be94f8
Alexandre Ferreira <ajpf44@gmail.com>
2024-07-21 09:27:59 -0300

    added the guess game

---
+++ guess-game/src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+use rand::Rng;
+use std::io::Write;
+
+fn main() {
+    println!("starting the guess game\nguess a number in a range 0-100");
+    let mut rng  = rand::thread_rng();
+    let guess_num:u8 = rng.gen_range(0..100);
+    let mut tries: u8 = 1;
+
+    while tries < 8 {
+        print!("type your guess: ");
+        io::stdout().flush().expect("Invalid flush");
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Falied to read the line");
+
+        let player_num = input
+            .trim()
+            .parse()
+            .expect("Not a number");
+
+
+        if guess_num > player_num {
+            println!("the number is greater");
+        }else if guess_num < player_num {
+            println!("the number is shorter");
+        }else { break;}
+
+        tries += 1;
+    }
+
+    if tries < 8 {
+        println!("You found the number in {} tries", tries);
+    } else {
+        println!("you got without tries. Loooser");
+    }
+}
